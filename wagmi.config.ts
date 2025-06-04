a9d27c996732ecea67ba1d66940f3c5419348122
Danilo Tuler <tuler@pobox.com>
2025-06-03 22:08:40 -0400

    feat: cartesi chain

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,24 +1,33 @@
-import * as fs from 'node:fs'
 import { defineConfig } from '@wagmi/cli'
 import { foundry } from '@wagmi/cli/plugins'
-import { anvil, base, baseSepolia, portoDev } from './src/core/Chains.js'
+import * as fs from 'node:fs'
+import {
+  anvil,
+  base,
+  baseSepolia,
+  cartesi,
+  portoDev,
+} from './src/core/Chains.js'
 import * as anvilAddresses from './test/src/_generated/addresses.js'
 
 const address = {
   exp1: {
     [anvil.id]: anvilAddresses.exp1Address,
+    [cartesi.id]: '0x700b6A60ce7EaaEA56F065753d8dcB9653dbAD35',
     [base.id]: '0x074C9c3273F31651a9dae896C1A1d68E868b6998',
     [baseSepolia.id]: '0x29f45fc3ed1d0ffafb5e2af9cc6c3ab1555cd5a2',
     [portoDev.id]: '0x97870b32890d3f1f089489a29007863a5678089d',
   },
   exp2: {
     [anvil.id]: anvilAddresses.exp2Address,
+    [cartesi.id]: '0xA15BB66138824a1c7167f5E85b957d04Dd34E468',
     [base.id]: '0xFcc74F42621D03Fd234d5f40931D8B82923E4D29',
     [baseSepolia.id]: '0x62a9d6de963a5590f6fba5119e937f167677bfe7',
     [portoDev.id]: '0x3a16de445711b203e472d1a04ac93ef7875cdf18',
   },
   expNft: {
     [anvil.id]: anvilAddresses.expNftAddress,
+    [cartesi.id]: '0xb19b36b1456E65E3A6D514D3F715f204BD59f431',
     [base.id]: '0xB37377508CbEd17a2B3694Fa0A68dc7CEE63DaF9',
     [baseSepolia.id]: '0xFcc74F42621D03Fd234d5f40931D8B82923E4D29',
     [portoDev.id]: '0x1bFf4BFA9b6bE201CEa697aa71c70522cD439d6D',
